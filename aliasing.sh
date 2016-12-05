393cb58fa0536b314c989dc1d98790ab65f03afe
monkeySee-monkeyCode <charly.dont.surf@gmail.com>
2016-12-05 03:53:50 -0500

    Create aliasing.sh

---
+++ aliasing.sh
@@ -0,0 +1,3 @@
+sudo echo "postmaster: webmaster@julians-mail.com" >> /etc/aliases
+sudo echo "root: webmaster@julians-mail.com" >> /etc/aliases
+sudo echo "www-data: webmaster@julians-mail.com" >> /etc/aliases
