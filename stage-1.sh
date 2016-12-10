993571b4bb87049c38fe856349d29ba95fcdea1f
monkeySee-monkeyCode <charly.dont.surf@gmail.com>
2016-12-09 22:19:52 -0500

    Update stage-1.sh

--- stage-1.sh
+++ stage-1.sh
@@ -1,7 +1,5 @@
 sudo apt-get install postfix
 
 sudo aptitude install python-software-properties
-sudo add-apt-repository ppa:malte.swart/dovecot-2.2
-sudo apt-get install dovecot
-sudo aptitude update
-sudo aptitude upgrade
+sudo apt-get install dovecot-imapd dovecot-pop3d
+
